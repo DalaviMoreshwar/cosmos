4b87f7b998b7130666986356b362e079f2b96371
Moreshwar Dalvi <moreshwardlv@gmail.com>
2023-12-13 01:27:59 +0530

    sign up page, banner, colors, form and validation

--- src/_auth/forms/SignUp.tsx
+++ src/_auth/forms/SignUp.tsx
@@ -1,7 +1,137 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { signUpValidationSchema } from "@/lib/validation";
+import { z } from "zod";
+import { Orbit } from "lucide-react";
+import Spin from "@/components/ui/shared/Spin";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const isLoading = false;
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof signUpValidationSchema>>({
+    resolver: zodResolver(signUpValidationSchema),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof signUpValidationSchema>) {
+    // create a user
+    // const newUser = await createUserAccount(values);
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <div className="sm:w-420">
+        <div className="text-center">
+          <div className="flex flex-center">
+            <Orbit size={28} strokeWidth={3} className="text-cyan-400" />
+            <span className="font-bold ml-2 h1-bold mb-0 text-slate-300">
+              Cosmos
+            </span>
+          </div>
+          <p className="text-light-3 mb-1 small-medium md:base-regular">
+            Short and global, suggests a vast network.
+          </p>
+          <h3 className="h3-bold md:h2-bold sm:pt-6">Create a new account</h3>
+        </div>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-5 w-full mt-1"
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">
+            {isLoading ? (
+              <div className="flex-center gap-2">
+                <Spin />
+                Loading...
+              </div>
+            ) : (
+              "Sign Up"
+            )}
+          </Button>
+
+          <p className="text-small-regular text-light-2 text-center">
+            Already have an account?
+            <Link
+              to="/sign-in"
+              className="text-primary-500 text-small-semibold ml-1"
+            >
+              Sign In
+            </Link>
+          </p>
+        </form>
+      </div>
+    </Form>
+  );
 };
 
 export default SignUp;
---
+++ src/components/ui/shared/Spin.tsx
@@ -0,0 +1,11 @@
+import { Loader } from "lucide-react";
+
+const Spin = () => {
+  return (
+    <div className="flex-center w-full">
+      <Loader strokeWidth={1.5} />
+    </div>
+  );
+};
+
+export default Spin;
