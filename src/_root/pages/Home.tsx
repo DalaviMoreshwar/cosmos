5b5e22ddbf4ea15fa685f579a244875f0bc0b600
Moreshwar Dalvi <moreshwardlv@gmail.com>
2023-12-18 15:10:23 +0530

    added block for no documents

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -2,6 +2,7 @@ import PostCard from "@/components/ui/shared/PostCard";
 import Spin from "@/components/ui/shared/Spin";
 import { useGetRecentPosts } from "@/lib/react-query/queriesAndMutations";
 import { Models } from "appwrite";
+import { Bird } from "lucide-react";
 
 const Home = () => {
   const {
@@ -11,6 +12,18 @@ const Home = () => {
   } = useGetRecentPosts();
 
   if (isError) return;
+
+  if (posts?.documents.length === 0)
+    return (
+      <>
+        <div className="home-container">
+          <h2 className="flex flex-col items-center text-light-4 my-auto md:h2-bold h-40 text-center w-full">
+            <Bird size="100" strokeWidth={1} />
+            No documents
+          </h2>
+        </div>
+      </>
+    );
 
   return (
     <div className="flex flex-1">
